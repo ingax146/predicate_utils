f42e23e3b9ea5fbb15ee75dd5851d39e69554f0a
Ingemar Axelsson <ingemar.axelsson@gmail.com>
2012-04-11 12:25:11 +0200

    Removed @Override annotations

--- src/com/emc/xcelerators/community/bpm/predicateutils/impl/converters/LessThanEqualConverter.java
+++ src/com/emc/xcelerators/community/bpm/predicateutils/impl/converters/LessThanEqualConverter.java
@@ -12,9 +12,9 @@ import com.emc.xcelerators.community.bpm.predicateutils.impl.BooleanConverter;
 public class LessThanEqualConverter implements BooleanConverter {
 
 	/**
+	 * @param result 
 	 * @return true if integer is less or equal to 0.
 	 */
-	@Override
 	public boolean convert(int result) {
 		return result <= 0;
 	}
